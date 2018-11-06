a115d4b088621e32879f85413258e769d6e640d9
sataivlis <yjie@ucmerced.edu>
2018-11-05 22:23:06 -0800

    Update lnp.py

--- lnp.py
+++ lnp.py
@@ -25,9 +25,52 @@ def createTable (c):
         sc_calcScore decimal(2,0) NOT NULL,
         sc_companyScore decimal(2,0) NOT NULL,
         sc_clearScore decimal(2,0) NOT NULL
-        );   ''')
+        ); ''')
 
 # create table stages;
+    c.execute(''' CREATE TABLE IF NOT EXISTS stages (s_chapterNumber integer PRIMARY KEY,
+        s_stars decimal(2,0) NOT NULL,
+        s_requireScore decimal(2,0) NOT NULL
+        ); ''')
+
+# create table stage R-cards;
+    c.execute(''' CREATE TABLE IF NOT EXISTS stageR (stgR_ID integer PRIMARY KEY,
+        stgR_cardD decimal(2,0) NOT NULL,
+        stgR_chapterNumber decimal(2,0) NOT NULL,
+        stgR_calcScore decimal(2,0) NOT NULL,
+        stgR_sumScore decimal(2,0) NOT NULL
+        ); ''')
+
+# create table stage R-cards;
+c.execute(''' CREATE TABLE IF NOT EXISTS stageR (stgR_ID integer PRIMARY KEY,
+    stgR_cardD decimal(2,0) NOT NULL,
+    stgR_chapterNumber decimal(2,0) NOT NULL,
+    stgR_calcScore decimal(2,0) NOT NULL,
+    stgR_sumScore decimal(2,0) NOT NULL
+    ); ''')
+
+# create table stage SR-cards;
+c.execute(''' CREATE TABLE IF NOT EXISTS stageSR (stgSR_ID integer PRIMARY KEY,
+    stgSR_cardD decimal(2,0) NOT NULL,
+    stgSR_chapterNumber decimal(2,0) NOT NULL,
+    stgSR_calcScore decimal(2,0) NOT NULL,
+    stgSR_sumScore decimal(2,0) NOT NULL
+    ); ''')
+
+# create table stage SSR-cards;
+c.execute(''' CREATE TABLE IF NOT EXISTS stageSSR (stgSSR_ID integer PRIMARY KEY,
+    stgSSR_cardD decimal(2,0) NOT NULL,
+    stgSSR_chapterNumber decimal(2,0) NOT NULL,
+    stgSSR_calcScore decimal(2,0) NOT NULL,
+    stgSSR_sumScore decimal(2,0) NOT NULL
+    ); ''')
+
+
+
+
+
+
+
 
 
 
